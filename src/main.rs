eb1f17691a7288be0dc0c5ce65a6430dbba07a56
Nicolas Pearson <nic.s.pearson@gmail.com>
2019-03-10 12:47:15 +0200

    Added Hero API Calls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::{Json, JsonValue};
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[get("/")]
+fn read() -> Json<JsonValue> {
+    Json(json!([
+        "hero 1", 
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<JsonValue> {
+    Json(json!({"status": "ok"}))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
+        .launch();
 }
